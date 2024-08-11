1587caa770681632b112a369342d9670e0aebd9a
David Mohar <david.mohar@shekaj.si>
2024-08-11 15:01:38 +0200

    chore: basic scaffold for nodejs webapps

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    mode: "development",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'nodejs-template',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
